ad940cde29bad63cbf7cdde0635325b4bfc8116c
pjames <pjames@gmail.com>
2025-08-31 07:28:32 +0700

    Added verbose message

--- scripts/scan-item-generator.rb
+++ scripts/scan-item-generator.rb
@@ -13,5 +13,15 @@ end
 $stdout.sync = true
 
 totalItem = ENV['SCAN_ITEM_COUNT']
+scanItemUrl = ENV['SCAN_ITEM_URL']
+scanItemBucket = ENV['SCAN_ITEM_BUCKET']
+scanItemOrg = ENV['SCAN_ITEM_ORG'] 
+
+puts("INFO : ### Start generating scan-items.")
+
+puts("INFO : ### SCAN_ITEM_COUNT=[#{totalItem}]")
+puts("INFO : ### SCAN_ITEM_URL=[#{scanItemUrl}]")
+puts("INFO : ### SCAN_ITEM_BUCKET=[#{scanItemBucket}]")
+puts("INFO : ### SCAN_ITEM_ORG=[#{scanItemOrg}]")
 
 puts("INFO : ### Done generating [#{totalItem}] items.")
