6000935f8223a4588dbc5f5f4bdfbecc75eef813
pjames <pjames@gmail.com>
2025-09-02 07:47:51 +0700

    Update serial number logic

--- scripts/utils.rb
+++ scripts/utils.rb
@@ -3,6 +3,11 @@ require "base64"
 require 'net/http'
 require 'redis'
 
+def random_char()
+  char = (65 + SecureRandom.random_number(26)).chr 
+  return char
+end
+
 def random_string(length = 7)
   chars = ('A'..'Z').to_a + ('0'..'9').to_a
   Array.new(length) { chars.sample }.join
@@ -24,4 +29,21 @@ end
 
 def escape_char(str)
   return "#{str}".tr("'", "")
+end
+
+def generate_serial(prefix:, digits:, start:)
+  # ตรวจสอบว่า prefix เป็นตัวอักษรใหญ่ตัวเดียว
+  unless prefix.match?(/\A[A-Z]\z/)
+    raise ArgumentError, "Prefix need to be only one capital letter (A-Z)"
+  end
+
+  # สร้าง format เช่น "%06d" ถ้า digits = 6
+  number_format = "%0#{digits}d"
+
+  # คืนค่าเป็น String ที่ prefix + running number
+  "#{prefix}#{format(number_format, start)}"
+end
+
+def fallback(param1, param2)
+  param1.nil? || param1.strip.empty? ? param2 : param1
 end
