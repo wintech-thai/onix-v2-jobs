a12db31181ca63ef10fef9f4ec63364216952000
pjames <pjames@gmail.com>
2026-07-21 18:51:25 +0700

    Implement Agent.Delete and Agent.Restart logic #2

--- scripts/job-dispatcher-line-agent.rb
+++ scripts/job-dispatcher-line-agent.rb
@@ -21,7 +21,7 @@ def get_yaml(param, appName)
 
   agentId   = param['AGENT_ID']
   agentCode = param['AGENT_CODE']
-  imageTag  = "v0.0.1" #param['AGENT_IMAGE_TAG']
+  imageTag  = param['AGENT_IMAGE_TAG']
   imageRepo = "asia-southeast1-docker.pkg.dev/its-artifact-commons/please-payment/please-payment-agent"
 
   endPointNotification = param['NOTIFICATION_ENDPOINT']
