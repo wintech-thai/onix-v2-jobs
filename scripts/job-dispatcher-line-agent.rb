d334a8b0480927f899614c9b70e116aba16cfc91
pjames <pjames@gmail.com>
2026-07-22 07:07:25 +0700

    Adjus env var for new line agent

--- scripts/job-dispatcher-line-agent.rb
+++ scripts/job-dispatcher-line-agent.rb
@@ -37,6 +37,7 @@ def get_yaml(param, appName)
     'ONIX_API_KEY'           => param['ENDPOINT_API_KEY'],
     'ONIX_ORG'               => 'global',
     'ONIX_API_URL'           => endPointNotification,
+    'API_BASE_URL'           => 'http://localhost:3000',
   }
 
   envYaml = envVars.map do |key, value|
